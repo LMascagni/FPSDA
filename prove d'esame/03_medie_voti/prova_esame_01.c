1b34a7d432e7f0413bbbd5dec6c0a82884c6b044
LMascagni <173911@spes.uniud.it>
2024-12-11 22:35:47 +0100

    Add student evaluation processing and output generation

--- prove d'esame/03_medie_voti/prova_esame_01.c
+++ prove d'esame/03_medie_voti/prova_esame_01.c
@@ -9,160 +9,220 @@
 
 struct date
 {
-    int anno;
-    int mese;
-    int giorno;
+   int anno;
+   int mese;
+   int giorno;
 };
 
 struct valutazione
 {
-    int voto;
-    struct date data;
+   int voto;
+   struct date data;
 };
 
 struct studente
 {
-    char nome[MAX_LEN];
-    char cognome[MAX_LEN];
+   char nome[MAX_LEN];
+   char cognome[MAX_LEN];
 
-    struct valutazione valutazioni[MAX_VOTI];
-    int nVoti;
+   struct valutazione valutazioni[MAX_VOTI];
+   int nVoti;
 };
 
-void parseData(FILE *fp_in, struct studente studenti[MAX_STUDENTI]);
+int parseData(FILE *fp_in, struct studente studenti[MAX_STUDENTI]);
 void leggiStringaDaFile(FILE *fp_in, char str[MAX_LEN]);
 int cercaStudente(struct studente studenti[MAX_STUDENTI], int n_studenti, char cognome[MAX_LEN], char nome[MAX_LEN]);
+struct date convertiData(char str[MAX_LEN]);
+void updateOutput (FILE *fp_out, struct studente studenti[MAX_STUDENTI], int n_studenti);
+
+void stampaDatiDebug(struct studente studenti[MAX_STUDENTI], int n_studenti);
 
 int main()
 {
-    // dichiarazione degli studenti
-    struct studente studenti[MAX_STUDENTI];
-    int n_studenti;
+   // dichiarazione degli studenti
+   struct studente studenti[MAX_STUDENTI];
+   int n_studenti;
 
-    n_studenti = 0;
+   n_studenti = 0;
 
-    // apertura dei file
-    FILE *fp_in;
-    // FILE *fp_out;
+   // apertura dei file
+   FILE *fp_in;
+   FILE *fp_out;
 
-    fp_in = fopen("in.txt", "r");
-    // fp_out = fopen("out.txt", "w");
+   fp_in = fopen("in.txt", "r");
+   fp_out = fopen("out.txt", "w");
 
-    // leggi file e "smista"
-    parseData(fp_in, studenti);
+   if((fp_in == NULL) || (fp_out == NULL))
+   {
+      printf("errore nell'apertura dei file");
+      exit(EXIT_FAILURE);
+   }
 
-    // scrivi il nuovo file con le informazioni degli studenti
-    // updateOutput ();
+   // leggi file e "smista"
+   n_studenti = parseData(fp_in, studenti);
 
-    return EXIT_SUCCESS;
+   // per debug stampa i dati su terminale
+   // * stampaDatiDebug(studenti, n_studenti);
+
+   // scrivi il nuovo file con le informazioni degli studenti
+   updateOutput (fp_out, studenti, n_studenti);
+
+   //chiusura dei file
+   fclose(fp_in);
+   fclose(fp_out);
+
+   return EXIT_SUCCESS;
 }
 
-void parseData(FILE *fp_in, struct studente studenti[MAX_STUDENTI])
+int parseData(FILE *fp_in, struct studente studenti[MAX_STUDENTI])
 {
-    int n_studenti = 0;
-    int matricola;
-    char nome[MAX_LEN];
-    char cognome[MAX_LEN];
-    char strData[MAX_LEN];
+   int n_studenti = 0;
+   int matricola;
+   char ch;
+   char nome[MAX_LEN];
+   char cognome[MAX_LEN];
+   char strData[MAX_LEN];
 
-    matricola = 0;
+   matricola = 0;
 
-    // esempio di riga da leggere
-    // Della Valle Di Sotto, Luigi, 7/12/2020, 5.
+   // esempio di riga da leggere
+   // Della Valle Di Sotto, Luigi, 7/12/2020, 5.
 
-    // while(il file non è finito)
-    while (!feof(fp_in))
-    {
-        // leggi il cognome (stringa)
-        leggiStringaDaFile(fp_in, cognome);
+   // while(il file non è finito)
+   while (!feof(fp_in))
+   {
+      // leggi il cognome (stringa)
+      leggiStringaDaFile(fp_in, cognome);
 
-        // leggi il nome (stringa)
-        leggiStringaDaFile(fp_in, nome);
+      // leggi il nome (stringa)
+      leggiStringaDaFile(fp_in, nome);
 
-        // ottieni la matricola
-        matricola = cercaStudente(studenti, n_studenti, cognome, nome);
+      // ottieni la matricola
+      matricola = cercaStudente(studenti, n_studenti, cognome, nome);
 
-        // salva nome e cognome se la matricola è nuova
-        if (matricola > n_studenti)
-        {
-            strcpy(studenti[matricola-1].cognome, cognome);
-            strcpy(studenti[matricola-1].nome, nome);
-            n_studenti++;
-        }
+      //printf("matricola Studente di %s %s: %d\n", cognome, nome, matricola);
 
-        // leggi la data (stringa da convertire)
-        leggiStringaDaFile(fp_in, strData);
-        // converti la data da stringa a numeri
-        //
+      // salva nome e cognome se la matricola è nuova
+      if (matricola >= n_studenti)
+      {
+         strcpy(studenti[matricola].cognome, cognome);
+         strcpy(studenti[matricola].nome, nome);
+         n_studenti++;
+      }
 
-        // leggi la valutazione
-        fscanf(fp_in, "%d", &studenti[matricola-1].valutazioni[0].voto); //!!!!!!!!!!!!!!!
-        printf("%d\n", n_studenti);
+      // leggi la data (stringa da convertire)
+      leggiStringaDaFile(fp_in, strData);
+      // converti la data da stringa a numeri
+      studenti[matricola].valutazioni[studenti[matricola].nVoti].data = convertiData(strData);
+      
+      // leggi la valutazione
+      fscanf(fp_in, "%d", &studenti[matricola].valutazioni[studenti[matricola].nVoti].voto);
+      studenti[matricola].nVoti++;
+      
+      //printf("\nn_studenti = %d\n\n", n_studenti);
 
-        fgetc(fp_in);
-        fgetc(fp_in);
-        fgetc(fp_in);
-    }
-    
-    printf("\nsium\n");
+      while ((ch = fgetc(fp_in)) == '.');
+      fgetc(fp_in);
+   }
 
-    for (matricola = 0; matricola < n_studenti; matricola++)
-    {
-        printf("---------------\nDATI LETTI\n");
-        printf("n: %d\n", matricola);
-        printf("cognome: %s\n", studenti[matricola].cognome);
-        printf("nome: %s\n", studenti[matricola].nome);
-        printf("data: %s\n", strData);
-        printf("voto: %d\n", studenti[matricola].valutazioni[0].voto);
-    }
+   return n_studenti;
+}
 
-    return;
+struct date convertiData(char str[MAX_LEN])
+{
+   struct date data;
+
+   sscanf(str, "%d/%d/%d", &data.giorno, &data.mese, &data.anno);
+
+   return data;
 }
 
 void leggiStringaDaFile(FILE *fp_in, char str[MAX_LEN])
 {
-    // finchè non leggo una virgola metto i caratteri nella stringa
+   // finchè non leggo una virgola metto i caratteri nella stringa
 
-    int i;
-    char ch;
+   int i;
+   char ch;
 
-    i = 0;
+   i = 0;
 
-    if ((ch = fgetc(fp_in)) == ' ')
-    {
-        while ((ch = fgetc(fp_in)) == ' ')
-            ;
-    }
+   // salto gli spazi iniziali
+   if ((ch = fgetc(fp_in)) == ' ')
+   {
+      while ((ch = fgetc(fp_in)) == ' ');
+   }
 
-    while (ch != ',')
-    {
-        str[i] = ch;
+   while (ch != ',')
+   {
+      str[i] = ch;
 
-        ch = fgetc(fp_in);
+      ch = fgetc(fp_in);
 
-        i++;
-    }
-    str[i] = '\0';
+      i++;
+   }
+   str[i] = '\0';
 
-    // printf("%s", str);
+   // printf("%s", str);
 }
 
 int cercaStudente(struct studente studenti[MAX_STUDENTI], int n_studenti, char cognome[MAX_LEN], char nome[MAX_LEN])
 {
-    int matricola;
-    matricola = 0;
+   int matricola;
+   matricola = 0;
 
-    while (matricola <= n_studenti)
-    {
-        if ((strcmp(cognome, studenti[matricola].cognome) == 0) && (strcmp(nome, studenti[matricola].nome) == 0))
-        {
-            printf("studente trovato!!");
-            return matricola;
-        }
+   while (matricola < n_studenti)
+   {
+      if ((strcmp(cognome, studenti[matricola].cognome) == 0) && (strcmp(nome, studenti[matricola].nome) == 0))
+      {
+         //studente trovato
+         return matricola;
+      }
 
-        matricola++;
-    }
+      matricola++;
+   }
 
-    printf("studente nuovo!!");
-    return matricola;
+   //nuovo studente
+   return matricola;
+}
+
+void stampaDatiDebug(struct studente studenti[MAX_STUDENTI], int n_studenti)
+{
+   printf("\nDATI STRUTTURA");
+   for (int matricola = 0; matricola < n_studenti; matricola++)
+   {
+      printf("\n---------------\n");
+      printf("matricola: %d\n", matricola);
+      printf("cognome: %s\n", studenti[matricola].cognome);
+      printf("nome: %s\n", studenti[matricola].nome);
+      for (int i = 0; i < studenti[matricola].nVoti; i++)
+      {
+         printf("data: %d/%d/%d \t|\t", studenti[matricola].valutazioni[i].data.giorno, 
+                                        studenti[matricola].valutazioni[i].data.mese, 
+                                        studenti[matricola].valutazioni[i].data.anno);
+         printf("voto: %d\n", studenti[matricola].valutazioni[i].voto);
+      }
+   }
+}
+
+void updateOutput (FILE *fp_out, struct studente studenti[MAX_STUDENTI], int n_studenti)
+{
+   double media;
+   for (int matricola = 0; matricola < n_studenti; matricola++)
+   {
+      media = 0;
+      //stampa nome e cognome
+      fprintf(fp_out, "%s %s\n", studenti[matricola].nome, studenti[matricola].cognome);
+      //stampa data e voto (per ogni voto), calcola la media
+      for (int i = 0; i < studenti[matricola].nVoti; i++)
+      {
+         fprintf(fp_out, "%d-%d-%d %d\n", studenti[matricola].valutazioni[i].data.anno, 
+                                          studenti[matricola].valutazioni[i].data.mese, 
+                                          studenti[matricola].valutazioni[i].data.giorno, 
+                                          studenti[matricola].valutazioni[i].voto);
+         media += studenti[matricola].valutazioni[i].voto;
+      }
+      //MEDIA: ...
+      media = media / (double)studenti[matricola].nVoti;
+      fprintf(fp_out, "MEDIA: %.1lf\n\n", media);
+   }
 }
