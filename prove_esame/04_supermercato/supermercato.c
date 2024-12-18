7c7721e38c7fc8e6b74b69c5ead2f9369c8521e1
Lorenzo Mascagni <126956415+LMascagni@users.noreply.github.com>
2024-12-18 15:37:31 +0100

    sium

--- prove_esame/04_supermercato/supermercato.c
+++ prove_esame/04_supermercato/supermercato.c
@@ -24,6 +24,14 @@ struct dimensioni
 };
 
 struct dimensioni parseData(FILE *fp, struct sezione scaffali[MAX_NF][MAX_NP][MAX_NZ]);
+void aggiornaSupermercato(FILE *fp, struct sezione scaffali[MAX_NF][MAX_NP][MAX_NZ], struct dimensioni dim);
+struct dimensioni cercaProdotto(struct sezione scaffali[MAX_NF][MAX_NP][MAX_NZ], struct dimensioni dim, char cod[MAX_LEN]);
+void aggiornaProdotto(struct sezione scaffali[MAX_NF][MAX_NP][MAX_NZ], struct dimensioni dim, struct dimensioni pos, int n);
+void aggiornaFileDisposizione(FILE *fp, struct sezione scaffali[MAX_NF][MAX_NP][MAX_NZ], struct dimensioni dim);
+
+#ifdef DEBUG
+void stampaSupermercato(struct sezione scaffali[MAX_NF][MAX_NP][MAX_NZ], struct dimensioni dim);
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -57,28 +65,17 @@ int main(int argc, char *argv[])
 
    // Stampa di debug per verificare la corretta lettura dei dati
 #ifdef DEBUG
-   int i, j, k;
-   for (i = 0; i < dim.nf; i++)
-   {
-      for (j = 0; j < dim.np; j++)
-      {
-         for (k = 0; k < dim.nz; k++)
-         {
-            if(strcmp(scaffali[i][j][k].codice, "") != 0)
-            {
-               printf("%d %d %d | %s  \t| %d %d %d\n", i, j, k, scaffali[i][j][k].codice, scaffali[i][j][k].Ne, scaffali[i][j][k].Nmax, scaffali[i][j][k].Nmag);
-            }
-            else
-            {
-               printf("%d %d %d | ------\n", i, j, k);
-            }
-         }
-      }
-   }
+   stampaSupermercato(scaffali, dim);
 #endif
    // Aggiorna l'array in base al file rifornimento
+   aggiornaSupermercato(fp_r, scaffali, dim);
+
+#ifdef DEBUG
+   stampaSupermercato(scaffali, dim);
+#endif
 
    // Scrivere le informazioni dell'array sul terzo file
+   aggiornaFileDisposizione(fp_sma, scaffali, dim);
 
    fclose(fp_sm);
    fclose(fp_r);
@@ -94,6 +91,7 @@ struct dimensioni parseData(FILE *fp, struct sezione scaffali[MAX_NF][MAX_NP][MA
    // Leggi la prima riga per le "dimensioni del supermercato"
    fscanf(fp, "%d %d %d", &dim.nf, &dim.np, &dim.nz);
 #ifdef DEBUG
+   printf("DATI LETTI\n");
    printf("Numero di file: %d\n", dim.nf);
    printf("Numero di pile: %d\n", dim.np);
    printf("Numero di zone: %d\n", dim.nz);
@@ -114,3 +112,102 @@ struct dimensioni parseData(FILE *fp, struct sezione scaffali[MAX_NF][MAX_NP][MA
 
    return dim;
 }
+
+void aggiornaSupermercato(FILE *fp, struct sezione scaffali[MAX_NF][MAX_NP][MAX_NZ], struct dimensioni dim)
+{
+   int n;
+   char cod[MAX_LEN];
+   struct dimensioni posizione;
+
+   while(fscanf(fp, "%s %d", cod, &n) != EOF)
+   {
+      posizione = cercaProdotto(scaffali, dim, cod);
+      aggiornaProdotto(scaffali, dim, posizione, n);
+   }
+
+   return;
+}
+
+struct dimensioni cercaProdotto(struct sezione scaffali[MAX_NF][MAX_NP][MAX_NZ], struct dimensioni dim, char cod[MAX_LEN])
+{
+   struct dimensioni pos;
+   for(pos.nf = 0; pos.nf < dim.nf; pos.nf++)
+   {
+      for(pos.np = 0; pos.np < dim.np; pos.np++)
+      {
+         for(pos.nz = 0; pos.nz < dim.nz; pos.nz++)
+         {
+            if(strcmp(cod, scaffali[pos.nf][pos.np][pos.nz].codice) == 0)
+            {
+               return pos;
+            }
+         }
+      }
+   }
+
+   return pos;
+}
+
+void aggiornaProdotto(struct sezione scaffali[MAX_NF][MAX_NP][MAX_NZ], struct dimensioni dim, struct dimensioni pos, int n)
+{
+   scaffali[pos.nf][pos.np][pos.nz].Ne += n;
+
+   if(scaffali[pos.nf][pos.np][pos.nz].Ne > scaffali[pos.nf][pos.np][pos.nz].Nmax)
+   {
+      scaffali[pos.nf][pos.np][pos.nz].Nmag += (scaffali[pos.nf][pos.np][pos.nz].Ne - scaffali[pos.nf][pos.np][pos.nz].Nmax);
+      scaffali[pos.nf][pos.np][pos.nz].Ne = scaffali[pos.nf][pos.np][pos.nz].Nmax;
+   }
+   return;
+}
+
+void aggiornaFileDisposizione(FILE *fp, struct sezione scaffali[MAX_NF][MAX_NP][MAX_NZ], struct dimensioni dim)
+{
+   //stampa delle dimensioni del supermercato
+   fprintf(fp, "%d %d %d\n", dim.nf, dim.np, dim.nz);
+
+   int i, j, k;
+
+   for (i = 0; i < dim.nf; i++)
+   {
+      for (j = 0; j < dim.np; j++)
+      {
+         for (k = 0; k < dim.nz; k++)
+         {
+            if(strcmp(scaffali[i][j][k].codice, "") != 0)
+            {
+               fprintf(fp, "%d %d %d %s %d %d %d\n", i, j, k, scaffali[i][j][k].codice, scaffali[i][j][k].Ne, scaffali[i][j][k].Nmax, scaffali[i][j][k].Nmag);
+            }
+         }
+      }
+   }
+
+   return;
+}
+
+#ifdef DEBUG
+void stampaSupermercato(struct sezione scaffali[MAX_NF][MAX_NP][MAX_NZ], struct dimensioni dim)
+{
+   int i, j, k;
+
+   printf("\nSTAMPA DELLA STRUTTURA\n");
+
+   for (i = 0; i < dim.nf; i++)
+   {
+      for (j = 0; j < dim.np; j++)
+      {
+         for (k = 0; k < dim.nz; k++)
+         {
+            if(strcmp(scaffali[i][j][k].codice, "") != 0)
+            {
+               printf("%d %d %d | %s  \t| %d %d %d\n", i, j, k, scaffali[i][j][k].codice, scaffali[i][j][k].Ne, scaffali[i][j][k].Nmax, scaffali[i][j][k].Nmag);
+            }
+            else
+            {
+               printf("%d %d %d | ------\n", i, j, k);
+            }
+         }
+      }
+   }
+   return;
+}
+#endif
